158fac1710c1072fee8135945ff7304718e8d09b
Eduardo Caires Silva <yandudu743@gmail.com>
2024-05-05 18:25:34 -0300

    Adicionando toda a lógica em 1 linha

--- js/script.js
+++ js/script.js
@@ -1,19 +1,17 @@
 const elements = {
-    botaoClique: document.getElementById('botao-clique'),
-    fraseInicial: document.getElementById('frase-inicial')
-}
+  botaoClique: document.getElementById("botao-clique"),
+  fraseInicial: document.getElementById("frase-inicial"),
+};
 
-let numeroAleatorio = Math.floor(Math.random() * 101)
+let numeroAleatorio = Math.floor(Math.random() * 101);
 
+elements.botaoClique.addEventListener("click", function (event) {
+  let numeroEscolhido = Number(prompt("Qual é o seu palpite?"));
 
-elements.botaoClique.addEventListener('click', function (event) {
-    
-    let numeroEscolhido = Number(prompt("Qual é o seu palpite?"))
-
-    numeroEscolhido === numeroAleatorio ? elements.fraseInicial.innerHTML = `<strong>PARABÉNS!</strong> Você acertou! Eu tinha sorteado o valor ${numeroAleatorio}! ${document.getElementById('botao-clique').style.visibility = 'hidden'}` : console.log("Você acertou!")
-
-    numeroEscolhido < numeroAleatorio ? elements.fraseInicial.innerHTML += `Você falou ${numeroEscolhido}. Meu número é <strong>MAIOR!</strong> <br>` : console.log("Você errou!")
-
-    numeroEscolhido > numeroAleatorio ? elements.fraseInicial.innerHTML += `Você falou ${numeroEscolhido}. Meu número é <strong>MENOR!</strong> <br>` : console.log("Você errou!")
-
-})
+  numeroEscolhido === numeroAleatorio
+    ? ((elements.fraseInicial.innerHTML = `<strong>PARABÉNS!</strong> Você acertou! Eu tinha sorteado o valor ${numeroAleatorio}!`),
+      (elements.botaoClique.style.visibility = "hidden"))
+    : numeroEscolhido < numeroAleatorio
+    ? (elements.fraseInicial.innerHTML += `Você falou ${numeroEscolhido}. Meu número é <strong>MAIOR!</strong> <br>`)
+    : (elements.fraseInicial.innerHTML += `Você falou ${numeroEscolhido}. Meu número é <strong>MENOR!</strong> <br>`);
+});
